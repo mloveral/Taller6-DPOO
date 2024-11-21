46443106103f8a148cca08269a596bd82f642f51
Manu_Love <107737902+mloveral@users.noreply.github.com>
2024-11-21 15:08:16 -0500

    Botones agregados

--- src/uniandes/dpoo/swing/interfaz/principal/PanelBotones.java
+++ src/uniandes/dpoo/swing/interfaz/principal/PanelBotones.java
@@ -32,9 +32,15 @@ public class PanelBotones extends JPanel implements ActionListener
 
         // Agrega el botón para crear un nuevo restaurante
         // TODO completar
+        butNuevo = new JButton( "Nuevo" );
+        butNuevo.addActionListener(this);
+        butNuevo.setActionCommand(NUEVO);
 
         // Agrega el botón para ver todos los restaurantes
         // TODO completar
+        butVerTodos = new JButton("Ver Todos");
+        butVerTodos.addActionListener(this);
+        butVerTodos.setActionCommand(VER);
     }
 
     @Override
